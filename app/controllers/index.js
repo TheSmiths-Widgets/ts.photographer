b3319f093a922e3f3c49d4364e4859c72a82c196
Matthias Benkort <matthias.benkort@gmail.com>
2015-06-26 11:15:17 +0200

    Example test app

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,16 @@
-function doClick(e) {
-    alert($.label.text);
+$.photographer.init({
+    fadeDuration: $.photographer.NORMAL,
+
+    animations: [
+        $.photographer.DISCARD_SWIPE_VERTICAL,
+        $.photographer.DISCARD_ROTATE,
+        $.photographer.ADD_SMOOTH,
+        $.photographer.CHANGE_FADEOUT
+    ]
+});
+
+function retrieve () {
+    Ti.API.warn(JSON.stringify($.photographer.retrievePictures()));
 }
 
 $.index.open();
